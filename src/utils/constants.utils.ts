12d1776db416e1a8b266ebb16018d29d3caec2d2
Loïc Combis <combis.loic@gmail.com>
2025-08-18 10:23:16 +0200

    Remove versions from base api URL

--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -1,5 +1,5 @@
 export class ConstantsUtils {
-  static readonly PROD_API_BASE_URL = 'https://api.equos.ai/v1';
-  static readonly STAGING_API_BASE_URL = 'https://staging-api.equos.ai/v1';
-  static readonly LOCAL_API_BASE_URL = 'http://localhost:3000/v1';
+  static readonly PROD_API_BASE_URL = 'https://api.equos.ai';
+  static readonly STAGING_API_BASE_URL = 'https://staging-api.equos.ai';
+  static readonly LOCAL_API_BASE_URL = 'http://localhost:3000';
 }
