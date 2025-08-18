54d299f14404c35b464ba4be8a178caa557bd5c6
Loïc Combis <combis.loic@gmail.com>
2025-08-18 08:47:12 +0200

    Allow temporary env based api url

--- src/equos.ts
+++ src/equos.ts
@@ -10,21 +10,35 @@ export class Equos {
   private sessionApi: EquosSession;
 
   private constructor(
+    private readonly env: 'prod' | 'staging' | 'local' = 'prod',
     private readonly apiKey: string,
     private readonly version: string = 'v1',
   ) {
-    this.http = new HttpUtils(
-      ConstantsUtils.API_BASE_URL,
-      this.version,
-      this.apiKey,
-    );
+    this.http = new HttpUtils(this.apiUrl, this.version, this.apiKey);
 
     this.avatarApi = new EquosAvatar(this.http);
     this.sessionApi = new EquosSession(this.http);
   }
 
-  static client(apiKey: string): Equos {
-    return new Equos(apiKey);
+  get apiUrl(): string {
+    switch (this.env) {
+      case 'prod':
+        return ConstantsUtils.PROD_API_BASE_URL;
+      case 'staging':
+        return ConstantsUtils.STAGING_API_BASE_URL;
+      case 'local':
+        return ConstantsUtils.LOCAL_API_BASE_URL;
+      default:
+        throw new Error(`Unknown environment: ${this.env}`);
+    }
+  }
+
+  static client(
+    env: 'prod' | 'staging' | 'local',
+    apiKey: string,
+    version: string = 'v1',
+  ): Equos {
+    return new Equos(env, apiKey, version);
   }
 
   get avatars() {
--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -1,3 +1,5 @@
 export class ConstantsUtils {
-  static readonly API_BASE_URL = 'https://api.equos.io/v1';
+  static readonly PROD_API_BASE_URL = 'https://api.equos.ai/v1';
+  static readonly STAGING_API_BASE_URL = 'https://staging-api.equos.ai/v1';
+  static readonly LOCAL_API_BASE_URL = 'http://localhost:3000/v1';
 }
