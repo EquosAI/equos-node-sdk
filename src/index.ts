b35d61d5afca304711e63b0615e2d8ec8dd27174
Loïc Combis <combis.loic@gmail.com>
2025-09-15 12:01:06 +0200

    export types and classes from index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export * from './equos';
 export * from './types/error.type';
+
+export * from './types/agent.type';
+export * from './types/avatar.type';
+export * from './types/session.type';
+
+export * from './apis/agent';
+export * from './apis/avatar';
+export * from './apis/session';
