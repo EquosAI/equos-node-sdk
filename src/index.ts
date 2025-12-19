165a3e7511a0637cfc83e07f9617251518756d6c
Loïc Combis <combis.loic@gmail.com>
2025-12-19 10:15:39 +0100

    Remove other dependencies

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// New client (recommended)
-export { EquosClient, EquosOptions } from './client';
-
-// Generated API exports (models and API classes)
-export * from './api/models';
-export * from './api/apis';
--- src/options.ts
+++
@@ -1,7 +0,0 @@
-export interface EquosOptions {
-  /**
-   * API endpoint to use (default: https://api.equos.ai)
-   * Can leave blank to use the default endpoint.
-   */
-  endpoint?: string;
-}
